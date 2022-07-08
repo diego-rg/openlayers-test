e5c304dae86676df17033e1345951f4e5dbdc412
eu <diego_thegarden@hotmail.com>
2022-07-08 19:44:25 +0200

    map test 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import CustomMap from "./components/CustomMap";
-import Card from "./components/Card";
 
 const App = () => {
   return (
     <div className="App">
       <div className="w-[900px] h-[900px] max-w-[100vw] max-h-[100vh] p-4">
-        <Card />
         <CustomMap />
       </div>
     </div>
